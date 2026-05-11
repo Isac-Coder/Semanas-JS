5ae3438dfa400e15e12e996e3f4b286d4d218d71
Isac <isaacalvarez0516@gmail.com>
2026-05-11 15:54:08 -0500

    add

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,33 +1,9 @@
-const name = prompt("¿Cuál es tu nombre?");
-const age1 = prompt("¿Cuál es tu edad?");
-const age = parseInt(age1);
-
-if (isNaN(age)) {
-    console.error("Error: Por favor, ingresa una edad válida en números.")
+const personaje = {
+    ID: "N001",
+    Nombre: "Zapato",
+    Precio: "2000"
 };
 
-if (age<18){
-    alert("Hola "+name+", eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!")
-};
+const lista = new set([1,1,2,2,3,3,4,5]);
 
-if (age>=18){
-    alert("Hola "+name+", eres mayor de edad. Prepárate para grandes oportunidades en el mundo de la programación!");
-};
-
-
-// otra forma de hacerlo
-
-const name = prompt("¿Cuál es tu nombre?");
-const age = parseInt(prompt("¿Cuál es tu edad?"));
-
-// 1. Manejo del error (se mantiene el IF por ser una validación solitaria)
-if (isNaN(age)) {
-    console.error("Error: Por favor, ingresa una edad válida en números.");
-} else {
-    // 2. Operador ternario para decidir el mensaje
-    const mensaje = age < 18 
-        ? `Hola ${name}, eres menor de edad. ¡Sigue aprendiendo!` 
-        : `Hola ${name}, eres mayor de edad. ¡Prepárate para grandes oportunidades!`;
-
-    alert(mensaje);
-}
+console.log(lista);
